669bed7405d26db447a31376cdf2a15b2e879e5c
Labutin Evgeniy <labeg@mail.ru>
2020-12-27 18:56:21 +0300

    try unsafe perm

--- ci/build_web.sh
+++ ci/build_web.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y glib2.0-dev libvips-dev
 
 cd './Clients/SampleSolution.Web'
 npm config set unsafe-perm true
-npm install
+npm install --unsafe-perm=true --allow-root
 chmod -R 777 ./node_modules/*
 npm run build
 cd -
