6e3c141c33d610e1645d5dfcd884cce720b5879c
Labutin Evgeniy <labeg@mail.ru>
2020-12-27 20:47:33 +0300

    test build

--- ci/build_web.sh
+++ ci/build_web.sh
@@ -3,14 +3,10 @@ set -ex # e - exit on error, x - print command before execution
 
 umask 000
 
-apt-get update && apt-get install -y glib2.0-dev libvips-dev
+# apt-get update && apt-get install -y glib2.0-dev libvips-dev
 
 cd './Clients/SampleSolution.Web'
-mkdir /root/.npm
-chmod -R 777 /root/.npm
-ls -lah /root
-npm config set unsafe-perm true
-npm install --unsafe-perm=true --allow-root
+npm install --unsafe-perm=true
 chmod -R 777 ./node_modules/*
 npm run build
 cd -
