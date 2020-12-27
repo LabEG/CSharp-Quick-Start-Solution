7304aad81c063a27cd832674cfdc24fd5e2d8c2f
Labutin Evgeniy <labeg@mail.ru>
2020-12-27 20:51:23 +0300

    test build

--- ci/build_web.sh
+++ ci/build_web.sh
@@ -3,7 +3,7 @@ set -ex # e - exit on error, x - print command before execution
 
 umask 000
 
-# apt-get update && apt-get install -y glib2.0-dev libvips-dev
+apt-get update && apt-get install -y glib2.0-dev # libvips-dev
 
 cd './Clients/SampleSolution.Web'
 npm install --unsafe-perm=true
