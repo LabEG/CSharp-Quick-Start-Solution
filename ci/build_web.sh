824fa4b7de60bc2933a54a31af75f52e5dec9377
Labutin Evgeniy <labeg@mail.ru>
2020-12-28 14:50:48 +0300

    test

--- ci/build_web.sh
+++ ci/build_web.sh
@@ -6,7 +6,7 @@ umask 000
 apt update && apt install -y glib2.0-dev libvips-dev
 
 cd './Clients/SampleSolution.Web'
-sudo npm install --unsafe-perm
+npm install --unsafe-perm=true --allow-root
 chmod -R 777 ./node_modules/*
 npm run build
 cd -
