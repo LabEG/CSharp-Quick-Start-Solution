0612c13d4e1793dbac41bac4ca52e188a9ecacab
Labutin Evgeniy <labeg@mail.ru>
2020-12-28 15:43:31 +0300

    test

--- ci/build_web.sh
+++ ci/build_web.sh
@@ -4,6 +4,7 @@ set -ex # e - exit on error, x - print command before execution
 umask 000
 
 apt update && apt install -y glib2.0-dev libvips-dev
+chmod 777 /root
 
 cd './Clients/SampleSolution.Web'
 npm install
